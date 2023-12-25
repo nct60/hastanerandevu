358a2e8e9ff98e49b7e6fe239c80435523cdffcf
nct60 <babacannecati@hotmail.com>
2023-12-25 23:47:54 +0300

    Create DoktorBransController.cs

---
+++ Controllers/DoktorBransController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace HastaneRandevuSistemi.Controllers
+{
+    public class DoktorBransController : Controller
+    {
+        public DoktorBransController()
+        {
+
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Ekle()
+        {
+
+            return View();
+        }
+
+       
+        
+
+
+
+
+    }
+
+
+
+
+
+
+}
+
